555d8744e3dbe2382c7394772a85645f9e1cb388
olganovichkova <58965002+olganovichkova@users.noreply.github.com>
2024-03-01 02:56:52 -0700

    fixed detail page

--- src/app/search/filters/page.tsx
+++ src/app/search/filters/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React from "react";
-import { Formik, Form, Field, FormikProps } from "formik";
-import axios from "axios";
+import { Field, Formik, Form, FormikProps } from "formik";
+import SampleCard from "@/components/samplecard/samplecard.component";
+import { useState } from "react";
+import { Sample, API } from "@/services/api";
 
 interface InitialValuesForSearchFilters {
   category: string;
@@ -15,86 +16,34 @@ interface InitialValuesForSearchFilters {
   storageRoom: string;
 }
 
-export default function SearchFilters() {
+export default function FilterSearch() {
+  const [samples, setSamples] = useState<Sample[]>([]);
+
   return (
     <div>
-      <div>
-        <Formik
-          initialValues={{
-            category: "",
-            collectorName: "",
-            advisorName: "",
-            collectionYear: "",
-            collectionReason: "",
-            sampleForm: "",
-            sampleType: "",
-            storageBuilding: "",
-            storageRoom: "",
-          }}
-          onSubmit={async (values, actions) => {
-            console.log(values);
-            actions.setSubmitting(true);
-            const result = await axios
-              .get(
-                `http://localhost:5000/samples?category=${values.category}&collectorName=${values.collectorName}&advisorName=${values.advisorName}&collectionYear=${values.collectionYear}`
-              )
-              .then(() => {
-                actions.setSubmitting(false);
-              });
-          }}
-        >
-          {(props: FormikProps<InitialValuesForSearchFilters>) => (
-            <Form>
-              <fieldset>
-                <Field
-                  type="text"
-                  name="collectionYear"
-                  placeholder="collection year"
-                />
-                <Field
-                  type="text"
-                  name="collectorName"
-                  placeholder="collector name"
-                />
-                <Field name="advisorName" as="select">
-                  <option value="Ben">Ben</option>
-                  <option value="Dave">Dave</option>
-                </Field>
-                <Field name="category" as="select">
-                  <option value="singleSpecimen">Single Specimen</option>
-                  <option value="collection">Collection</option>
-                </Field>
-                <Field name="collectionReason" as="select">
-                  <option value="teaching">Teaching</option>
-                  <option value="research">Research</option>
-                </Field>
-                <Field name="sampleType" as="select">
-                  <option value="rock">Rock</option>
-                  <option value="mineral">Mineral</option>
-                  <option value="fossil">Fossil</option>
-                  <option value="soil">Soil</option>
-                  <option value="water">Water</option>
-                </Field>
-                <Field name="sampleForm" as="select">
-                  <option value="handSample">Hand Sample</option>
-                  <option value="mineralSeparate">Mineral Separate</option>
-                  <option value="thinSection">Thin Section</option>
-                </Field>
-                <Field name="storageBuilding" as="select">
-                  <option value="CH">CH</option>
-                  <option value="PS">PS</option>
-                </Field>
-                <Field name="storageRoom" as="select">
-                  <option value="room1">Room1</option>
-                  <option value="room2">Room2</option>
-                  <option value="room3">Room3</option>
-                </Field>
-                <button type="submit">SUBMIT</button>
-              </fieldset>
-            </Form>
-          )}
-        </Formik>
-      </div>
+      <Formik
+        initialValues={{
+          category: "",
+          collectorName: "",
+          advisorName: "",
+          collectionYear: "",
+          collectionReason: "",
+          sampleForm: "",
+          sampleType: "",
+          storageBuilding: "",
+          storageRoom: "",
+        }}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          actions.setSubmitting(true);
+          API.searchByFilter(values).then((result) => {
+            setSamples(result);
+            actions.setSubmitting(false);
+          });
+        }}
+      >
+        {(props: FormikProps<InitialValuesForSearchFilters>) => <div></div>}
+      </Formik>
     </div>
   );
 }
--- src/app/search/sampleDetail/[id]/page.tsx
+++ src/app/search/sampleDetail/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function SampleDetails({ params }: Props) {
     API.getSample(params.id).then((result) => {
       setSample(result);
     });
-  }, []);
+  }, [params.id]);
 
   if (!sample) {
     return <div>Loading...</div>;
@@ -31,12 +31,14 @@ export default function SampleDetails({ params }: Props) {
                 <div>
                   <div className="px-4 sm:px-0">
                     <h3 className="text-base font-semibold leading-7 text-gray-900">
-                      Sample ID
-                      {sample.sampleId}
+                      {sample.sampleId ? (
+                        `${sample.sampleId}`
+                      ) : (
+                        <div>Template id</div>
+                      )}
                     </h3>
                     <span className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-                      category
-                      {/* {sample.category} */}
+                      {sample.category}
                     </span>
                   </div>
                   <div className="mt-6 border-t border-gray-300">
@@ -51,7 +53,11 @@ export default function SampleDetails({ params }: Props) {
                               Collector Name
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              collectorname
+                              {sample.collectorName ? (
+                                `${sample.collectorName}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
 
@@ -60,7 +66,11 @@ export default function SampleDetails({ params }: Props) {
                               Advisor Name
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              Ben
+                              {sample.advisorName ? (
+                                `${sample.advisorName}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
 
@@ -69,15 +79,41 @@ export default function SampleDetails({ params }: Props) {
                               Collection Year
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              XXXX
+                              {sample.collectionYear ? (
+                                `${sample.collectionYear}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <div className="text-sm font-medium leading-6 text-gray-900">
                               Collection Site Coordinates
+                              {sample.locationMarkerlat ? " (Marker)" : ""}
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              XXXX
+                              <div>
+                                lat:
+                                {sample.locationMarkerlat ? (
+                                  `${sample.locationMarkerlat}`
+                                ) : (
+                                  <span className="text-gray-300 justify-self-end">
+                                    {" "}
+                                    xx.xxxx{" "}
+                                  </span>
+                                )}
+                              </div>
+                              <div>
+                                lng:
+                                {sample.locationMarkerlng ? (
+                                  `${sample.locationMarkerlng}`
+                                ) : (
+                                  <span className="text-gray-300 justify-self-end">
+                                    {" "}
+                                    xx.xxxx{" "}
+                                  </span>
+                                )}
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -92,25 +128,37 @@ export default function SampleDetails({ params }: Props) {
                               Short Description
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              description
+                              {sample.shortDescription ? (
+                                `${sample.shortDescription}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
 
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <div className="text-sm font-medium leading-6 text-gray-900">
-                              Type(s)
+                              Type (s)
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              {sample.sampleType?.join(", ")}
+                              {sample.sampleType ? (
+                                `${sample.sampleType.join(", ")}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
 
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <div className="text-sm font-medium leading-6 text-gray-900">
-                              Form(s)
+                              Form (s)
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              hand sample
+                              {sample.sampleForm ? (
+                                `${sample.sampleForm.join(", ")}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                         </div>
@@ -125,7 +173,11 @@ export default function SampleDetails({ params }: Props) {
                               Building
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              CH
+                              {sample.storageBuilding ? (
+                                `${sample.storageBuilding}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -133,7 +185,11 @@ export default function SampleDetails({ params }: Props) {
                               Room
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              room1
+                              {sample.storageRoom ? (
+                                `${sample.storageRoom}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -141,7 +197,11 @@ export default function SampleDetails({ params }: Props) {
                               Extra Details
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              ....
+                              {sample.storageDetails ? (
+                                `${sample.storageDetails}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                           <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -149,7 +209,11 @@ export default function SampleDetails({ params }: Props) {
                               Duration
                             </div>
                             <div className="mt-1 text-sm justify-self-end leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                              10 years
+                              {sample.storageDuration ? (
+                                `${sample.storageDuration}`
+                              ) : (
+                                <div className="text-gray-300"> --N/A-- </div>
+                              )}
                             </div>
                           </div>
                         </div>
