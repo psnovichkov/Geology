89681af28865372a5955a325c096ac191cb3de4b
olganovichkova <58965002+olganovichkova@users.noreply.github.com>
2024-03-02 01:39:11 -0700

    fixed redirecting of pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/search",
+        destination: "/search/term",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
